8dd91fa349662dc6c4821dcf36482666eca098b0
David Ilie <david@davidilie.com>
2023-03-16 10:09:48 +0000

    yay

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,11 +5,13 @@ import {
    View,
    Image,
    TouchableOpacity,
+   Animated,
 } from "react-native";
 import { SlideModal } from "react-native-slide-modal";
 
 import { Loading } from "./components/Loading";
 import { useWS } from "./components/WebSocketContext";
+import PinchPan from "./components/PinchPan";
 
 const Home: React.FC = () => {
    const { ws } = useWS();
@@ -32,7 +34,6 @@ const Home: React.FC = () => {
    useEffect(() => {
       if (ws.lastMessage.data) {
          const parsed = JSON.parse(ws.lastMessage.data);
-         console.log(parsed);
          switch (parsed.action) {
             case "getGameData":
                setGameData(parsed.res);
@@ -56,45 +57,67 @@ const Home: React.FC = () => {
          modalVisible={gameData?.firstConnection!}
          screenContainer={
             <SafeAreaView className="h-full w-full">
-               <View className="p-2">
-                  <Text className="text-lg text-center">
-                     Total Points:{" "}
-                     <Text className="font-bold text-blue-500">
-                        {gameData?.completed}
+               <View className="bg-white z-50 border-b pb-1 border-gray-300">
+                  <View className="p-2">
+                     <Text className="text-lg text-center">
+                        Total Points:{" "}
+                        <Text className="font-bold text-blue-500">
+                           {gameData?.completed}
+                        </Text>
                      </Text>
-                  </Text>
-                  <View className="flex-row mx-auto">
-                     <TouchableOpacity
-                        className={`px-2 py-1 ${
-                           floor === 1 ? "bg-gray-100" : "bg-blue-500"
-                        } rounded-l-md`}
-                        disabled={floor === 1}
-                        onPress={() => setFloor(1)}
-                     >
-                        <Text
-                           className={`text-lg ${
-                              floor === 2 ? "text-white" : "text-gray-300"
-                           }`}
+                     <View className="flex-row mx-auto mt-2">
+                        <TouchableOpacity
+                           className={`px-2 py-1 ${
+                              floor === 1 ? "bg-gray-100" : "bg-blue-500"
+                           } rounded-l-md`}
+                           disabled={floor === 1}
+                           onPress={() => setFloor(1)}
                         >
-                           Floor 1
-                        </Text>
-                     </TouchableOpacity>
-                     <TouchableOpacity
-                        className={`px-2 py-1 ${
-                           floor === 2 ? "bg-gray-100" : "bg-blue-500"
-                        } rounded-r-md`}
-                        disabled={floor === 2}
-                        onPress={() => setFloor(2)}
-                     >
-                        <Text
-                           className={`text-lg ${
-                              floor === 1 ? "text-white" : "text-gray-300"
-                           }`}
+                           <Text
+                              className={`text-lg ${
+                                 floor === 2 ? "text-white" : "text-gray-300"
+                              }`}
+                           >
+                              Floor 1
+                           </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                           className={`px-2 py-1 ${
+                              floor === 2 ? "bg-gray-100" : "bg-blue-500"
+                           } rounded-r-md`}
+                           disabled={floor === 2}
+                           onPress={() => setFloor(2)}
                         >
-                           Floor 2
-                        </Text>
-                     </TouchableOpacity>
+                           <Text
+                              className={`text-lg ${
+                                 floor === 1 ? "text-white" : "text-gray-300"
+                              }`}
+                           >
+                              Floor 2
+                           </Text>
+                        </TouchableOpacity>
+                     </View>
                   </View>
+               </View>
+               <View>
+                  <PinchPan>
+                     {({ scale, x, y }) => (
+                        <Animated.Image
+                           source={require("../assets/1st_floor_mansion_PHOTOSHOPPED.png")}
+                           style={{
+                              width: "80%",
+                              height: "80%",
+                              aspectRatio: 1,
+                              resizeMode: "contain",
+                              transform: [
+                                 { scale },
+                                 { translateX: x },
+                                 { translateY: y },
+                              ],
+                           }}
+                        />
+                     )}
+                  </PinchPan>
                </View>
             </SafeAreaView>
          }
@@ -104,11 +127,13 @@ const Home: React.FC = () => {
                   Welcome!
                </Text>
                <Text className="text-center mt-2">
-                  You have been given 10 random places around the school, try
+                  You have been given different points around the school, try
                   and find them...
                </Text>
                <Text className="text-center mt-2">
-                  First one to 10 points,{" "}
+                  The team which gets the most{" "}
+                  <Text className="text-blue-500 font-bold">points</Text> in 30
+                  minutes,{" "}
                   <Text className="text-blue-500 font-bold">wins!</Text>
                </Text>
                <View className="mt-48 mx-auto">
---
+++ app/src/components/PinchPan.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Animated } from "react-native";
+import {
+   PanGestureHandler,
+   PinchGestureHandler,
+   State,
+} from "react-native-gesture-handler";
+
+type InjectedProps = {
+   scale: Animated.Value;
+   x: Animated.Value;
+   y: Animated.Value;
+};
+
+const PinchPan: React.FC<{
+   children: (props: InjectedProps) => React.ReactNode;
+}> = ({ children }) => {
+   const scale = new Animated.Value(2);
+   const pan = new Animated.ValueXY({ x: 50, y: 50 });
+
+   let prevPanX = 0;
+   let prevPanY = 0;
+
+   return (
+      <PanGestureHandler
+         onGestureEvent={Animated.event(
+            [
+               {
+                  nativeEvent: {
+                     translationX: pan.x,
+                     translationY: pan.y,
+                  },
+               },
+            ],
+            { useNativeDriver: true }
+         )}
+         onHandlerStateChange={(event) => {
+            if (event.nativeEvent.oldState === State.ACTIVE) {
+               prevPanX += event.nativeEvent.translationX;
+               prevPanY += event.nativeEvent.translationY;
+               pan.setOffset({ x: prevPanX, y: prevPanY });
+               pan.setValue({ x: 0, y: 0 });
+            }
+         }}
+         minPointers={1}
+         maxPointers={1}
+      >
+         <Animated.View>
+            <PinchGestureHandler
+               onGestureEvent={Animated.event(
+                  [
+                     {
+                        nativeEvent: {
+                           scale,
+                        },
+                     },
+                  ],
+                  { useNativeDriver: true }
+               )}
+            >
+               {children({ scale, x: pan.x, y: pan.y })}
+            </PinchGestureHandler>
+         </Animated.View>
+      </PanGestureHandler>
+   );
+};
+
+export default PinchPan;
